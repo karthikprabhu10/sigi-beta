574842c4b8deea59daecc222beb0affed0e445ac
K4x <108575187+karthikprabhu10@users.noreply.github.com>
2024-11-19 22:28:54 +0530

    index.html

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleButton = document.querySelector('.navbar .toggle-button');
+  const navbarLinks = document.querySelector('.navbar .nav-links');
+
+  toggleButton.addEventListener('click', function() {
+    navbarLinks.classList.toggle('active');
+  });
+});
